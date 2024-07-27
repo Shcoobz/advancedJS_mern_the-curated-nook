b97b63faadebda07c557540f4f93b98baad8dab0
Shcoobz <a.boeckmann23@gmail.com>
2024-07-27 19:15:56 +0200

    implemented spinner component

---
+++ frontend/src/components/common/Spinner.jsx
@@ -0,0 +1,11 @@
+import PulseLoader from 'react-spinners/PulseLoader';
+
+function Spinner() {
+  return (
+    <div className='spinner-container'>
+      <PulseLoader color='currentColor' />
+    </div>
+  );
+}
+
+export default Spinner;
