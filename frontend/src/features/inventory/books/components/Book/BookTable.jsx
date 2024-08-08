09312c1c7b84b43533b4b2b6dd3bcf520fa256a9
Shcoobz <a.boeckmann23@gmail.com>
2024-08-08 14:13:53 +0200

    truncated description for books in list view

--- frontend/src/features/inventory/books/components/Book/BookTable.jsx
+++ frontend/src/features/inventory/books/components/Book/BookTable.jsx
@@ -1,13 +1,14 @@
 import { DEFAULT } from '../../../../../config/common/constants';
+import { useDispatch } from 'react-redux';
+import { useDeleteBookMutation } from '../../api/booksApiSlice';
+import { apiSlice } from '../../../../../app/api/apiSlice';
+import { handleDeleteEntityList } from '../../../../utils/formUtils';
+import { TOAST } from '../../../../../config/common/messages';
+import { truncateText } from '../../../../utils/utils';
 import {
   TableCell,
   TableCellActions,
 } from '../../../../../components/common/TableComponents';
-import { useDispatch } from 'react-redux';
-import { useDeleteBookMutation } from '../../api/booksApiSlice';
-import { apiSlice } from '../../../../../app/api/apiSlice';
-import { handleDeleteEntityList, isUUID } from '../../../../utils/formUtils';
-import { TOAST } from '../../../../../config/common/messages';
 
 function formatCategories(categories) {
   return categories.join(DEFAULT.commaSpace);
@@ -17,7 +18,8 @@ function BookTable({ book, onEdit }) {
   const [deleteBook] = useDeleteBookMutation();
   const dispatch = useDispatch();
   const bookCategoryList = formatCategories(book.categories);
-  const displayIsbn = isUUID(book.isbn) ? 'N/A' : book.isbn;
+
+  const truncatedDescription = truncateText(book.description, 298);
 
   async function handleDelete(e) {
     await handleDeleteEntityList(deleteBook, book.id, TOAST.SUCCESS.BOOK.deleted);
@@ -28,7 +30,7 @@ function BookTable({ book, onEdit }) {
   return (
     <>
       <TableCell className='book__title' content={book.title} />
-      <TableCell className='book__description' content={book.description} />
+      <TableCell className='book__description' content={truncatedDescription} />
       <TableCell className='book__categories' content={bookCategoryList} />
 
       <TableCellActions
--- frontend/src/features/utils/utils.js
+++ frontend/src/features/utils/utils.js
@@ -1,3 +1,11 @@
 export function replaceHtmlEntities(str) {
   return str.replace(/&quot;/g, '"');
 }
+
+export function truncateText(description, maxLength = 250) {
+  if (description.length > maxLength) {
+    return description.substring(0, maxLength) + ' [...]';
+  }
+
+  return description;
+}
